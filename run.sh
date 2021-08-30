c39c99a339577000dd2e44438de46d3e5e17188d
Katarina Tukelic <tukelic.katarina@gmail.com>
2021-08-30 23:10:07 +0200

    Change port

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ docker run \
     --name nistagram-frontend \
     -v ${PWD}:/app \
     -v /app/node_modules \
-    -p 3001:3000 \
+    -p 3000:3000 \
     -e CHOKIDAR_USEPOLLING=true \
     frontend:1.0
